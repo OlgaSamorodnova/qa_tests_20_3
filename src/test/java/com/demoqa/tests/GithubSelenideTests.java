dc1c8eb276caa814b65df532962fdebb5380e175
OlgaSamorodnova <118749608+OlgaSamorodnova@users.noreply.github.com>
2023-08-24 14:51:19 +0300

    SelenideTest

---
+++ src/test/java/com/demoqa/tests/GithubSelenideTests.java
@@ -0,0 +1,63 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Browsers.CHROME;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GithubSelenideTests {
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("#### Settings");
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = CHROME;
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    String JUnit5ExtendTestExample = "@ExtendWith({SoftAssertsExtension.class})\n" +
+            "class Tests {\n" +
+            "  @Test\n" +
+            "  void test() {\n" +
+            "    Configuration.assertionMode = SOFT;\n" +
+            "    open(\"page.html\");\n" +
+            "\n" +
+            "    $(\"#first\").should(visible).click();\n" +
+            "    $(\"#second\").should(visible).click();\n" +
+            "  }\n" +
+            "}";
+    String JUnit5RegisterTestExample = "class Tests {\n" +
+            "  @RegisterExtension \n" +
+            "  static SoftAssertsExtension softAsserts = new SoftAssertsExtension();\n" +
+            "\n" +
+            "  @Test\n" +
+            "  void test() {\n" +
+            "    Configuration.assertionMode = SOFT;\n" +
+            "    open(\"page.html\");\n" +
+            "\n" +
+            "    $(\"#first\").should(visible).click();\n" +
+            "    $(\"#second\").should(visible).click();\n" +
+            "  }\n" +
+            "}";
+
+    @Test
+    void successTest() {
+//        - Откройте страницу Selenide в Github
+//        - Перейдите в раздел Wiki проекта
+//        - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+//        - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").sendKeys("so");
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(Condition.text(JUnit5ExtendTestExample));
+        $("#wiki-body").shouldHave(Condition.text(JUnit5RegisterTestExample));
+    }
+}
