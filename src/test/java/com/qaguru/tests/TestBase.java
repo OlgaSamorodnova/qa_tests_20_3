96482ccb9ea78cdd8f89cd355929b4650ab83c64
OlgaSamorodnova <118749608+OlgaSamorodnova@users.noreply.github.com>
2024-06-11 16:31:11 +0300

    Homework jenkins

--- src/test/java/com/qaguru/tests/TestBase.java
+++ src/test/java/com/qaguru/tests/TestBase.java
@@ -1,7 +1,12 @@
 package com.qaguru.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Browsers.CHROME;
 
@@ -13,7 +18,16 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.browser = CHROME;
         Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         // Configuration.holdBrowserOpen = true;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
 }
